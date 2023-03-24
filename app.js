70b07cfd4a4dd388cd93cab735de128853ac9f2f
Prajjwal998 <118663769+Prajjwal998@users.noreply.github.com>
2023-03-24 17:17:15 +0530

    add google sheet to html form to receive message

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
+var sidemenu = document.getElementById("sidemenu");
 
 function opentab(tabname){
   for(tablink of tablinks){
@@ -11,3 +12,21 @@ function opentab(tabname){
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab");
 }
+
+function openmenu(){
+  sidemenu.style.right = "0";
+}
+function closemenu(){
+  sidemenu.style.right = "-200px";
+}
+
+// google  sheet add
+// const scriptURL = 'https://script.google.com/macros/s/AKfycby8P88aA9y-A407msJv2P3luhvTlHs3hbk9Xlc5GnuOMZrOa3rML07pN_61a5J1rPY/exec'
+// const form = document.forms['submit-to-google-sheet']
+
+// form.addEventListener('submit', e => {
+//   e.preventDefault()
+//   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//     .then(response => console.log('Success!', response))
+//     .catch(error => console.error('Error!', error.message))
+// })
